aa0b71054ea063af24aa783670db9cac19d5804d
zhenlei_sun <zhenlei_sun@sun.local>
2014-11-29 19:11:52 +1300

    修改页脚
    1. 增加页脚服务菜单；
    2. 增加页脚导航菜单；
    3. 修改页脚显示样式；

--- wp-content/themes/yueboke/footer.php
+++ wp-content/themes/yueboke/footer.php
@@ -1,8 +1,18 @@
 </section>
 <footer class="footer">
     <div class="footer-inner">
-        <div class="copyright pull-left">
-            版权所有，保留一切权利！ &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+    	<div class="footer-service">
+			<ul class="nav">
+				<?php echo str_replace("</ul></div>", "", ereg_replace("<div[^>]*><ul[^>]*>", "", wp_nav_menu(array('theme_location' => 'pagemenu', 'echo' => false, 'depth'=>0)) )); ?>
+			</ul>
+		</div>
+		<div class="footer-nav">
+			<ul class="nav">
+				<?php echo str_replace("</ul></div>", "", ereg_replace("<div[^>]*><ul[^>]*>", "", wp_nav_menu(array('theme_location' => 'nav', 'echo' => false, 'depth'=>0)) )); ?>
+			</ul>
+		</div>
+    	<div class="copyright">
+            2001-2014 All right reserved &copy; <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
         </div>
         <div class="trackcode pull-right">
             <?php if( dopt('d_track_b') ) echo dopt('d_track'); ?>
