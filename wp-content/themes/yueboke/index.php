5536f4aa118aa721b26a51ebf0cbe82855773616
wavegao <359261788@qq.com>
2014-12-01 09:57:03 +1300

    catagory

--- wp-content/themes/yueboke/index.php
+++ wp-content/themes/yueboke/index.php
@@ -21,9 +21,53 @@
 		    'paged' => $paged
 		);
 		query_posts($args);
-		include 'modules/excerpt.php';
+		/*include 'modules/excerpt.php';
+		*/
 		
 	?>
 	</div>
 </div>
+
+
+<div id="primary" class="content-area col-md-12"  align="center">
+		<!--<div id="home-title">
+			<span><?php _e('Recent Posts','verge'); ?><span>
+		</div>-->
+		<main id="main" class="site-main" role="main">
+		<?php $count = 0; ?>
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					if($count == 0)	
+						echo "<div class='row'>" ;
+					elseif($count%4 == 0)
+						echo "</div><!--.row--><div class='row'>";
+ 
+                    include 'content-grid4.php';
+					/*get_template_part( 'content', 'grid4' );*/	
+					$count++;		
+					
+				?>
+			<?php endwhile; ?>
+			<?php echo "</div><!--.row-->"; ?>
+			
+			<?php verge_pagination(); ?>
+
+		<?php else : ?>
+
+			<!--<?php get_template_part( 'content', 'none' ); ?>-->
+			<?php echo 'test'; ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
 <?php get_footer(); ?>
